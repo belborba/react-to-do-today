a9e970ef4e055bd82a17590363e68b594d972df9
Bel Borba <isaborba.cs@gmail.com>
2025-07-01 16:21:06 -0300

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { ThemeButton } from "../ThemeButton/ThemeButton";
 
 export const Header = () => {
   return (
-    <div className=" grow w-full flex justify-between border-zinc-300 dark:border-zinc-500 border-b-2 pb-2">
+    <div className="w-full flex flex-col items-center space-y-4 sm:flex-row sm:justify-between sm:items-start sm:space-y-0 border-zinc-300 dark:border-zinc-500 border-b-2 pb-2">
       <LogoLight className="text-zinc-800 dark:text-lime-300" />
       <div className="flex gap-3 items-center">
         <MessageButton />
